1682c414d7605fd2496ff304353659f1f6c0a7d7
ArpitRathi1 <rathi844@gmail.com>
2023-12-21 15:47:00 +0530

    added voting rights program

---
+++ voting_rights.py
@@ -0,0 +1,15 @@
+# Ques) Write a python program to find if a person is Elegible for voting or not.
+# Input-1-age
+# Output-not required.
+# process-
+# 1) Take the of age form the user and allocate its value in age.
+# 2) If age is greater then or equal to 18 then print Eligible for voting.
+# 3) else print not elegible for voting.
+
+age=int(input("Enter the age of person:"))
+
+if age>=18:
+	print("Eligible for voting")
+else:
+	print("Not eligible for voting:")
+	
