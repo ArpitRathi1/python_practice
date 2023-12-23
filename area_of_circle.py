60331cf7a045816a25180f53b3dcd10c61cb84d9
ArpitRathi1 <rathi844@gmail.com>
2023-12-23 15:14:09 +0530

    added area of circle

---
+++ area_of_circle.py
@@ -0,0 +1,13 @@
+# Ques) Write a python program to find area of circle.
+
+# Solution-
+# Input-1-radius_of_circle
+# Output-1-area_of_circle
+# process-
+# 1) Take a user defined value of radius of circle and allocated its value in radius_of_circle.
+# 2) Find area of circle by 22/7*(radius_of_circle)**2 and allocate its value in area_of_circle.
+# 3) print area_of_circle.
+
+radius_of_circle=float(input("Enter the value of radius_of_circle:"))
+area_of_circle=22/7*radius_of_circle**2
+print("Area of the circle is:"area_of_circle)
