7759e964d175530f1f8270d4a8582af1d027245e
ArpitRathi1 <rathi844@gmail.com>
2023-12-23 17:18:12 +0530

    correction in profit loss

--- profit_loss.py
+++ profit_loss.py
@@ -1,4 +1,4 @@
-# Ques) If the cost price and selling price of an item is input through the keyboard, write a program to determine whether the seller has made profit or incurred loss or no profit no loss. also determine how much profit he made or loss	he incurred.
+# Ques) If the cost price and selling price of an item is input through the keyboard, write a program to determine whether the seller has made profit or incurred loss or no profit no loss. also determine how much profit he made or loss he incurred.
 
 cost_price=int(input("Enter the cost price of the item:"))
 selling_price=int(input("Enter the selling price of the item:"))
