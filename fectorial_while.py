8dea8a6a7355e8d93b2f6a749b2343ca415af766
ArpitRathi1 <rathi844@gmail.com>
2023-12-26 13:59:29 +0530

    added fectorial while

---
+++ fectorial_while.py
@@ -0,0 +1,14 @@
+# Ques) write a python program to find fectorial of a user defined integer number.
+
+var=int(input("Enter any number:"))
+if var==0:
+	print("Fectorial of",var,"is=1")
+elif var<0:
+	print("Invalid number")
+else:
+	i=1
+	result=1
+	while i<=var:
+		result=result*i
+		i+=1
+	print("Fectorial of",var,"is=",result)
