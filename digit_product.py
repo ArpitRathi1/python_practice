ed103e0dc71eecc17b738ec34c4c2a1a035d4cf3
ArpitRathi1 <rathi844@gmail.com>
2023-12-27 12:34:43 +0530

    added digit product

---
+++ digit_product.py
@@ -0,0 +1,9 @@
+# Write a python program to display product of digits of the number accepted from the user.
+
+num=int(input("Enter any number:"))
+product=1
+while num:
+	last_digit=num%10
+	product=product*last_digit
+	num=num//10
+print("Product of digits of",num,"is =",product)
