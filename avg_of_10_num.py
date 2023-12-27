3cbf7abcd138738568a8b73bca361b22eb138901
ArpitRathi1 <rathi844@gmail.com>
2023-12-27 13:16:50 +0530

    added avg program

---
+++ avg_of_10_num.py
@@ -0,0 +1,9 @@
+# Ques) Write a python program which accept 10 numbers and display their average.
+
+numbers=[]
+for i in range (10):
+	num=float(input("Enter number:"))
+	numbers.append(num)
+print(numbers)
+addition=sum(numbers)
+print("Average of following 10 numbers is =",addition/10)
