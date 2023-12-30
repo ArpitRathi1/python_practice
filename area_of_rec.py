791b328c12ba9f5d9d72cb9f07a37824a3294c53
ArpitRathi1 <rathi844@gmail.com>
2023-12-30 10:09:09 +0530

    added area of rectengle

---
+++ area_of_rec.py
@@ -0,0 +1,6 @@
+# Write a python program to calculate area of a rectengle.
+
+length_of_rec=float(input("Enter the value of length_of_rec:"))
+width_of_rec=float(input("Enter the value of width_of_rec:"))
+area_of_rec=length_of_rec*width_of_rec
+print(area_of_rec)
