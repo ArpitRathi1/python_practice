1032b870b5d55aab3f516d86d3c71aa3b190f990
ArpitRathi1 <rathi844@gmail.com>
2023-12-30 10:12:17 +0530

    correction in area of rec

--- area_of_rec.py
+++ area_of_rec.py
@@ -3,4 +3,4 @@
 length_of_rec=float(input("Enter the value of length_of_rec:"))
 width_of_rec=float(input("Enter the value of width_of_rec:"))
 area_of_rec=length_of_rec*width_of_rec
-print(area_of_rec)
+print("Area of the rectengle is =",area_of_rec)
