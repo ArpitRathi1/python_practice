ade7fcb85514cefd8ed9bc382102ea3abad35734
ArpitRathi1 <rathi844@gmail.com>
2024-01-01 14:20:10 +0530

    added mark price

---
+++ mark_price.py
@@ -0,0 +1,19 @@
+# Accept the mark price from the user and calculate the net ammount to pay according to following criteria:
+# Marked price            discount
+# >10000                  20%
+# >7000 and <=10000       15%
+# <=7000                  10%
+
+mark_price=float(input("Enter the mark price of the item:"))
+if mark_price>10000:
+	discount=mark_price*(20/100)
+	net_ammount=mark_price-discount
+	print(net_ammount)
+elif mark_price>7000 and mark_price<=10000:
+	discount=mark_price*(15/100)
+	net_ammount=mark_price-discount
+	print(net_ammount)
+else:
+	discount=mark_price*(10/100)
+	net_ammount=mark_price-discount
+	print(net_ammount)
