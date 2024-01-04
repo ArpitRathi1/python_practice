638dbb17999b5053273150b15ffe9134bfbc08e7
ArpitRathi1 <rathi844@gmail.com>
2024-01-04 12:26:03 +0530

    added possible triangle

---
+++ possibility_of_triangle.py
@@ -0,0 +1,10 @@
+# Ques) write a python program to check a triangle is possible or not with the given length of sides.
+
+side_1=float(input("Enter length of side 1:"))
+side_2=float(input("Enter length of side 2:"))
+side_3=float(input("Enter length of side 3:"))
+
+if (side_1+side_2)>side_3 and (side_2+side_3)>side_1 and (side_3+side_1)>side_2:
+	print("Making of a triangle is possible with is sides.")
+else:
+	print("Making of a triangle is not possible with is sides.")
