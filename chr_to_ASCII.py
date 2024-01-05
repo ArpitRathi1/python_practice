cb5de1368b4b883fb579a20bcd720914dd6c3c8e
ArpitRathi1 <rathi844@gmail.com>
2024-01-05 09:57:43 +0530

    added chr to ascii

---
+++ chr_to_ASCII.py
@@ -0,0 +1,5 @@
+# Ques) Write a python program to find ASCII value of a user defined character.
+
+n=input("Enter character:")
+ascii_value=ord(n)
+print("ASCII value of {n} is = {a}".format(n=n,a=ascii_value))
