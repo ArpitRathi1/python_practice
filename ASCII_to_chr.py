3a5d0e802f03272f60ad6ceced5d33d613c4ad27
ArpitRathi1 <rathi844@gmail.com>
2024-01-05 09:59:44 +0530

    ascii to chr

---
+++ ASCII_to_chr.py
@@ -0,0 +1,5 @@
+# Write a python program to give character of a ASCII number accepted by the user.
+
+Ascii=int(input("Enter Ascii code:"))
+character=chr(Ascii)
+print("Character of {a} is = {c}".format(a=Ascii,c=character))
