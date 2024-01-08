22377c29f395e7a441a6d3f98e75961fd63ea2f5
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-01-08 11:03:02 +0530

    Update greatest_number.py

--- greatest_number.py
+++ greatest_number.py
@@ -1,4 +1,6 @@
 # Ques) Write a python program to print the greatest number among 3 user defined numbers.
+# Solution-
+
 n1=float(input("Enter number one:"))
 n2=float(input("Enter number two:"))
 n3=float(input("Enter number three:"))
