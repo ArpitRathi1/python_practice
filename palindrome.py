0915d45b47c53d75eac29aeb2479d30e5759fee3
ArpitRathi1 <rathi844@gmail.com>
2024-01-09 09:51:22 +0530

    added palindrome

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+# Write a python program to check the given input is a palindrome or not.
+
+input_string=input("Enter input_string:")
+reverse=""
+for i in reversed(input_string):
+	reverse=reverse+i
+print(reverse)
+if input_string==reverse:
+	print("It is a palindrome.")
+else:
+	print("It is not a palindrome.")
