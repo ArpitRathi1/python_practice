042b225ca4999b388f5dc3d054b7156f3740bc60
ArpitRathi1 <rathi844@gmail.com>
2024-01-10 11:18:36 +0530

    added bonus

---
+++ bonus.py
@@ -0,0 +1,15 @@
+# Ques) Write a python program to calculate bonus according to salary and time period of service of employes.
+
+
+salary=int(input("Enter your salary:"))
+time=int(input("Enter time period of service:"))
+
+if time>10:
+	bonus=salary*(10/100)
+	print("your bonus ammount is {}".format(bonus))
+elif time>=6 and time<=10:
+	bonus=salary*(8/100)
+	print("your bonus ammount is {}".format(bonus))
+else:
+	bonus=salary*(5/100)
+	print("your bonus ammount is {}".format(bonus))
