abb5af04ab7e9877683af0db2620c08947bf2b85
ArpitRathi1 <rathi844@gmail.com>
2024-01-11 10:19:59 +0530

    added fibonacii series

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+# Ques) Write a python program to print fibonacci series.
+
+n=int(input("Enter n:"))
+num_1=0
+num_2=1
+for i in range(n):
+	print(num_1)
+	res=num_1+num_2
+	num_1=num_2
+	num_2=res
