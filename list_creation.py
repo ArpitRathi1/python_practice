4dea2163a20e91307acfb4cd508bf09fc6a88b9f
ArpitRathi1 <rathi844@gmail.com>
2024-01-13 15:17:24 +0530

    added list creation

---
+++ list_creation.py
@@ -0,0 +1,16 @@
+# Ques) Write a python script to create a list of:
+# 1) Four vegetables 
+veg=["Apple","Banana","Cherry","Guava"]
+print(veg)
+# 2) Vowels
+vowels=["A","E","I","O","U"]
+print(vowels)
+# 3) First 10 natural numbers.
+num=[1,2,3,4,5,6,7,8,9,10]
+print(num) 
+# 4) Square of first 5 numbers
+sq=[1,4,9,16,25]
+print(sq)
+# 5) First 5 multiples of 6.
+mul_6=[6,12,18,24,30]
+print(mul_6)
