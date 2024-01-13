8d4b6974a4c8b44a8fef2f64954670fb0b735dc4
ArpitRathi1 <rathi844@gmail.com>
2024-01-13 15:08:19 +0530

    temp based ordering

---
+++ temp_based_ordering.py
@@ -0,0 +1,18 @@
+# Input-1- temp
+# Output- not required
+# Process-
+# 1) Take a user defined value of tempreture and allocate it in temp.
+# 2) If value in temp is less then or equal to 20 then print I will have coffee.
+# 3) Else if value in temp is more then 20 and less then or equal to 25 then print I will have tea.
+# 4) Else print I will have cold coffee.
+
+temp=float(input("Enter temp:"))
+
+if temp<=20:
+	print("I will have coffee")
+
+elif temp>20 and temp<=25:
+	print("I will have tea")
+
+else:
+	print("I will have cold coffee")
