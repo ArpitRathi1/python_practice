2dfba479980e6395945b72a0ad4b5226dbc489eb
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-01-14 15:04:00 +0530

    Update maximum_list.py

--- maximum_list.py
+++ maximum_list.py
@@ -1,4 +1,6 @@
-# Ques) write a program to find maximum number in a list without using max function.
+# Ques) Write a program to find maximum number in a list without using max function in the given list:
+# lst=[12,54,234,78673,42,5567,32]
+# Solution-
 
 lst=[12,54,234,78673,42,5567,32]
 maximum=lst[0]
