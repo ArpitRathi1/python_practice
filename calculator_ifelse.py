afdc9cac2b4048e1b4d471c1d500876d70a73a92
ArpitRathi1 <rathi844@gmail.com>
2024-01-15 12:50:45 +0530

    added calculator if else

---
+++ calculator_ifelse.py
@@ -0,0 +1,29 @@
+num_1=float(input("Enter num_1:"))
+num_2=float(input("Enter num_2:"))
+
+oprator=input("Enter oprator:")
+
+if oprator=="+":
+	print("Addtion is=",num_1+num_2)
+
+elif oprator=="-":
+	print("Difference is=",num_1-num_2)
+
+elif oprator=="*":
+	print("Multiplication is=",num_1*num_2)
+
+elif oprator=="/":
+	print("Division is=",num_1/num_2)
+
+elif oprator=="//":
+	print("Floor division is=",num_1//num_2)
+
+elif oprator=="%":
+	print("Remainder is=",num_1%num_2)
+
+elif oprator=="**":
+	print("result is=",num_1**num_2)
+
+else:
+	print("Invalid oprator")
+
