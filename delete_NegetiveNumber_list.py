b00946d58efd90fa28187b3346165ef85b74ddc1
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-01-16 10:12:30 +0530

    Update delete_NegetiveNumber_list.py

--- delete_NegetiveNumber_list.py
+++ delete_NegetiveNumber_list.py
@@ -1,4 +1,6 @@
-# Ques) Write a python program to delete all the negative numbers from a list.
+# Ques) Write a python program to delete all the negative numbers from the given list:
+# l1=[-1,-3,9,6,0,2,-6,-11]
+# Solution-
 
 l1=[-1,-3,9,6,0,2,-6,-11]
 positive=[]
