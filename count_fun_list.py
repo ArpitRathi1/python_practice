93e4112e0d2e281d809a98add9641503cfee664e
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-01-16 10:10:03 +0530

    Update count_fun_list.py

--- count_fun_list.py
+++ count_fun_list.py
@@ -1,5 +1,6 @@
 # Ques) Write a program to input a number and count the occurrence of that number in the given list.
 # l1=[31,21,3,12,31,56,76,5,4,21,12,34]
+# Solution-
 
 l1=[31,21,3,12,31,56,76,5,4,21,12,34]
 num=float(input("Enter any number : "))
