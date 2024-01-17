1195d67771dcfe5bb317757fc8061fd0e1f53bcd
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-01-17 11:13:17 +0530

    Update number_present_in_list.py

--- number_present_in_list.py
+++ number_present_in_list.py
@@ -1,4 +1,6 @@
 # Ques) Write a python program to check whether a number (accepted by the user) is present in the list or not.
+# Solution-
+
 l1=[1,2,3,4,5,6,7,8,9]
 num=float(input("Enter any number : "))
 if num in l1:
