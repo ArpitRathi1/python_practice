b6e8a5dc4057b5f7f3d801c913addef1160bfb6f
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-01-24 17:38:39 +0530

    Update tuple_functions.py

--- tuple_functions.py
+++ tuple_functions.py
@@ -1,33 +1,34 @@
 # Creating a tuple.
 fruits=("Mango","apple","kiwi")
 fruits_1=("guava","orange")
-# creating tuple with one item-you should add a , and the end.
-"""fruit=("guava",)
-fruit_1=tuple("banana")"""
 
-# check type.
-"""print(type(fruits))
+# Creating tuple with one item-you should add a , and the end.
+fruit=("guava",)
+fruit_1=tuple("banana")
+
+# Check type.
+print(type(fruits))
 print(type(fruit))
-print(type(fruit_1))"""
+print(type(fruit_1))
 
-# check length of tuple.
-"""print(len(fruits))"""
+# Check length of tuple.
+print(len(fruits))
 
 # Accessing items in the tuple.
-"""print(fruits[2]) # positive indexing
-print(fruits[-1]) # negative indexing
-print(fruits[1:3]) # range indexing
-print(fruits[-2:]) # negative range indexing"""
+print(fruits[2])     # positive indexing
+print(fruits[-1])    # negative indexing
+print(fruits[1:3])   # range indexing
+print(fruits[-2:])   # negative range indexing
 
-# traverse a tuple
-"""for i in fruits:
-	print(i)"""
+# Traverse a tuple
+for i in fruits:
+	print(i)
 
 # Concatenate two tuples.
-"""fruits=fruits+fruits_1
-print(fruits)"""
+fruits=fruits+fruits_1
+print(fruits)
 
 # Unpacking a tuple.
-"""fruit1,fruit2,fruit3=fruits
+fruit1,fruit2,fruit3=fruits
 print(fruit1)
-print(fruit2)"""
+print(fruit2)
