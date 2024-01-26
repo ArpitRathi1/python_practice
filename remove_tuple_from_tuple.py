709fa1fd4579fd65be6ca66d57bcec405faf534e
ArpitRathi1 <rathi844@gmail.com>
2024-01-26 12:25:15 +0530

    added remove element from tuple

---
+++ remove_tuple_from_tuple.py
@@ -0,0 +1,12 @@
+# Ques) Write a python program to remove (accepted from the user) from the given tuple
+# T1=(12,15,18,21,24,27,19)
+
+T1=(12,15,18,21,24,27,19)
+el=int(input("Enter the elements to be removed : "))
+if el in T1:
+	l=list(T1)
+	l.remove(el)
+	T1=tuple(l)
+	print(T1)
+else:
+	print("Element not found.")
