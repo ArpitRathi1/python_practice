b24fa2014763197de421f718668ab292c399ba88
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-01-29 15:29:54 +0530

    Update reverse_tuple.py

--- reverse_tuple.py
+++ reverse_tuple.py
@@ -1,13 +1,12 @@
+# Ques) Write a python program to reverse the given tuple ages=(10,30,15,17,65,11,9) 
+# Solution-
+
 ages=(10,30,15,17,65,11,9)
-
 list=[]
-
 # Adding values in a list.
 for i in reversed(ages):
 	list.append(i)
-
 # Typecasing list into tuple.
 Output_tuple=tuple(list)
-
 # Printing output_tuple.
 print(Output_tuple)
