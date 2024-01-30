7380abbe611c0bd7ff87245ac5b7655af1ecbf74
ArpitRathi1 <rathi844@gmail.com>
2024-01-30 11:05:30 +0530

    added prime number between two number

---
+++ prime_no_between_2_no.py
@@ -0,0 +1,22 @@
+# Ques)  Write a program to print all prime number fall between two numbers including both.(accept two numbers from user)
+
+num_1=int(input("Enter first number:"))
+num_2=int(input("Enter second number:"))
+extra=[]
+prime=[]
+non_prime=[]
+for i in range(num_1,(num_2+1)):
+    if i == 0:
+        extra.append(i)
+    elif i == 1:
+        non_prime.append(i)
+    else:
+        for j in range(2,i):
+            if i%j==0:
+                non_prime.append(i)
+                break
+        else:
+            prime.append(i)
+print("Prime number-",prime)
+print("Non-prime-",non_prime)
+print("Neither prime nor Non-prime-",extra)
