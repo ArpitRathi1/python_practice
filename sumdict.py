df84a3d12b7c1f6348719e3e8148a8765b6df1f1
ArpitRathi1 <rathi844@gmail.com>
2024-02-02 10:35:26 +0530

    added sumdict

---
+++ sumdict.py
@@ -0,0 +1,10 @@
+# Write a python program to find sum of all items in a given dictionary.
+# dict={"a":100,"b":200,"c":300}
+dict={
+	"a":100,
+	"b":200,
+	"c":300
+}
+
+print(dict.values())
+print("The sum of dict values is",sum(dict.values()))
