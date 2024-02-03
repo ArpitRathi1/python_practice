0f4cff3392cf71d9ca89bce8040c7ef59f77c3af
ArpitRathi1 <rathi844@gmail.com>
2024-02-03 12:31:12 +0530

    added number of words in a sentence

---
+++ number_of_words_in_sentence.py
@@ -0,0 +1,15 @@
+# Write a python program to find how many repeated words in the given sentence.
+# string="my name is Arpit my city is indore"
+
+string="my name is Arpit my city is indore"
+lst=string.split()
+value=0
+dic = {}
+dic=dict.fromkeys(lst,value)
+print(dic)
+
+for i in lst:
+    #dic[i] = dic.get(i,0)+1
+    if i in lst:
+        dic[i]=dic[i]+1
+print(dic)
