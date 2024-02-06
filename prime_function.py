6d844d84140b1d73eb69ea4bd371b6f1603cf3bd
ArpitRathi1 <rathi844@gmail.com>
2024-02-06 12:42:45 +0530

    added prime function

---
+++ prime_function.py
@@ -0,0 +1,20 @@
+# Ques) Write a python function to find the user defined number is prime or not.
+
+def prime (x):
+	if x==1:
+		return False
+	else:
+		for i in range(2,x):
+			if x%i==0:
+				return False
+				break
+		else:
+			return True
+			
+n=int(input("Enter n : "))
+for i in range (1,(n+1)):
+	output=prime(i)
+	if output==True:
+		print(i,"is a prime number.")
+	else:
+		print(i,"is a non-prime number.")
