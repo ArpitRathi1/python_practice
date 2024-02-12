602b16cc3e8e12ff4e54e0d8f79538fc16d60ed5
ArpitRathi1 <rathi844@gmail.com>
2024-02-12 11:00:36 +0530

    added string manipulation

---
+++ string_manipulation.py
@@ -0,0 +1,22 @@
+st_1="new york"
+# upper()-to convert characters of the string to upper-case.
+st_2=st_1.upper()
+print(st_2)
+
+# lower()-to convert characters of the string to lower-case.
+st_3=st_2.lower()
+print(st_3)
+
+# Note-upper() and lower will ignore other characters apart from alphabates.
+s="abc123"
+s1=s.upper()
+print(s1)
+
+# capitalize()- To capitalize first character of string.
+st_4=st_1.capitalize()
+print(st_4)
+
+# strip()- for stripping/removing any trailing whitespaces.
+st_5="    hello world   "
+print(st_5.strip())
+print(st_5)          # strip() will not change anything in original string.
