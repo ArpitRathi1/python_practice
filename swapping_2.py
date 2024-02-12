b64f2d6f8a2a93c187344b467e11f61633dc7a5a
ArpitRathi1 <rathi844@gmail.com>
2024-02-12 11:04:08 +0530

    added swapping 2

---
+++ swapping_2.py
@@ -0,0 +1,11 @@
+# Ques) Write a python script to swap two variable.
+
+a=input("Enter any number or string : ")
+b=input("Enter any number or string : ")
+print("a :",a)
+print("b :",b)
+
+a,b=b,a
+
+print("a :",a)
+print("b :",b)
