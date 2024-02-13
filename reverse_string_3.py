dbd0fe17d5c971bda580056f9319a3dda3bec05e
ArpitRathi1 <rathi844@gmail.com>
2024-02-13 10:32:41 +0530

    added reverse string 3

---
+++ reverse_string_3.py
@@ -0,0 +1,6 @@
+# Ques) Write a python script to reverse a string.
+
+input_string=input("Enter input string : ")
+reversed_string=input_string[::-1]
+print("Input String : ",input_string)
+print("Reversed String : ",reversed_string)
