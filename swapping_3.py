17a816414f6190d8e05087c016016e4bc2c8923a
ArpitRathi1 <rathi844@gmail.com>
2024-02-14 11:02:20 +0530

    added swapping 3

---
+++ swapping_3.py
@@ -0,0 +1,14 @@
+# Ques) Write a python program to swap two numbers without using a third variable.
+
+a=int(input("Enter number one : "))
+b=int(input("Enter number two : "))
+
+print("First number : ",a)
+print("Second number : ",b)
+
+a=a+b
+b=a-b
+a=a-b
+
+print("First number : ",a)
+print("Second number : ",b)
