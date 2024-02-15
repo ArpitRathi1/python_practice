f1ecdb31c0de306d1ad47655768ced63939aa61d
ArpitRathi1 <rathi844@gmail.com>
2024-02-15 09:58:14 +0530

    upadted

---
+++ string_practice.py
@@ -0,0 +1,20 @@
+# Consider a string "The unexpected always happens"
+
+# Ques 1) Assign this string a variable text
+text="The unexpected always happens."
+# Ques 2) Print this string.
+print(text)
+# Ques 3) Print length of the string.
+print(len(text))
+# Ques 4) Check if "pp" present in the string,
+print("pp" in text)
+# Ques 5) Print substring form 0 till 10.
+print(text[0:11])
+# Ques 6) Replace always with never
+text_1=text.replace("always","never")
+print(text_1)
+# Ques 7) Add "no matter what."
+text_3="no matter what."
+text_4=text + text_3
+# Ques) Print the final string.
+print(text_4)
