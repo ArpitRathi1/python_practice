db8457247723a874c167a23388df1e25b4c68c79
ArpitRathi1 <rathi844@gmail.com>
2024-02-15 09:47:16 +0530

    added string formatting

---
+++ string_formet.py
@@ -0,0 +1,7 @@
+name="Arpit"
+age=26
+education="Engg"
+
+print("hy,I am",name,"I am",age,"years old","and I am an",education)
+print("hy,I am "+name+" I am "+str(age)+" years old and I am an " + education)
+print("Hi I am {}. I am {} years old. By profession I am {}.".format(name,age,education))
