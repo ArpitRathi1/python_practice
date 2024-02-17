c2c097fb996923a6c19aca1d179292707122e301
ArpitRathi1 <rathi844@gmail.com>
2024-02-17 12:22:43 +0530

    added leap year function

---
+++ functions/leap_year_function.py
@@ -0,0 +1,17 @@
+def leap_year(n):
+	if n%100==0:
+		if n%400==0:
+			return True
+		else:
+			return False
+	else:
+		if n%4==0:
+			return True
+		else:
+			return False
+year=int(input("Enter year :"))
+output=leap_year(year)
+if output==True:
+	print(year,"is a leap year")
+else:
+	print(year,"is not a leap year")
