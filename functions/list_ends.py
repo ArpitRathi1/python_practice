c1b865c758fdb7be062bef8f69706f6a9b8765d2
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-02-19 12:34:21 +0530

    Update list_ends.py

--- functions/list_ends.py
+++ functions/list_ends.py
@@ -1,4 +1,5 @@
 # Ques) Write a function that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. 
+# Solution-
 
 def list_ends(lst):
 	b=[lst[0],lst[-1]]
