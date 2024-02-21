24f10e5c16068b0caa125ba73d01136c47d92b26
ArpitRathi1 <rathi844@gmail.com>
2024-02-21 09:30:10 +0530

    update tuple questions

---
+++ tuple_questions/add_3_number_in_tuple.py
@@ -0,0 +1,10 @@
+# Ques) Write a python script to add three numbers form the user and insert it at the end of the given series t1=(1,2,3,4,5,6) .
+# Solution-
+
+t1=(1,2,3,4,5,6)
+t2=()
+for i in range (3):
+	num=float(input("Enter any number to add in tuple : "))
+	t2=t2+(num,)
+t1=t1+t2
+print(t1)
---
+++ tuple_questions/add_marks_in_tuple.py
@@ -0,0 +1,9 @@
+# Ques) Write a python program to find sum of marks of all student stored in the given tuple.
+# t1=(("Suman",35),("Arpit",75),("Ankit",50))
+# Solution-
+
+t1=(("Suman",35),("Arpit",75),("Ankit",50))
+add=0
+for i in t1:
+	add=add+i[1]
+print("Sum of marks is = ",add)
