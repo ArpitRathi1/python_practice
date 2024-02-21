10c56fc6744c051088bbc5b37722b794a56167ab
ArpitRathi1 <rathi844@gmail.com>
2024-02-21 09:27:18 +0530

    added new folder

--- first_10_multilple_in_tuple.py
+++
@@ -1,9 +0,0 @@
-# Ques) Write a python program to accpet a number from the user and store first 10 multiples of that number in a tuple.
-# Solution-
-
-n=int(input("Enter any number : "))
-tup=()
-for i in range (1,11):
-	res=n*i
-	tup=tup+(res,)
-print(tup)
