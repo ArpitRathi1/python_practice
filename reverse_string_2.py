862ab1f9e4c74b503cf809a2b5e1303b75d6d85f
ArpitRathi1 <rathi844@gmail.com>
2024-02-24 12:47:06 +0530

    moved

--- reverse_string_2.py
+++
@@ -1,9 +0,0 @@
-# Ques) Write a python script to reverse a string.
-# Solution-
-
-input_string=input("Enter input string : ")
-reversed_string=""
-for i  in range ((len(input_string)-1),-1,-1):
-	reversed_string=reversed_string+input_string[i]
-print("Input string : ",input_string)
-print("Reversed string : ",reversed_string)
--- reverse_string_3.py
+++
@@ -1,7 +0,0 @@
-# Ques) Write a python script to reverse a string.
-# Solution-
-
-input_string=input("Enter input string : ")
-reversed_string=input_string[::-1]
-print("Input String : ",input_string)
-print("Reversed String : ",reversed_string)
