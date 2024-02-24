f0dbd65350909c7b64dd76c9a8e59f88ce2371eb
ArpitRathi1 <rathi844@gmail.com>
2024-02-24 12:50:39 +0530

    moved

--- string_formet.py
+++
@@ -1,9 +0,0 @@
-# Different ways of string formatting.
-
-name="Arpit"
-age=26
-education="Engg"
-
-print("hy,I am",name,"I am",age,"years old","and I am an",education)
-print("hy,I am "+name+" I am "+str(age)+" years old and I am an " + education)
-print("Hi I am {}. I am {} years old. By profession I am {}.".format(name,age,education))
