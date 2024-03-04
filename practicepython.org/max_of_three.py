ef67422ebb7c9c361a630b37f154d08526f4d9b7
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-03-04 18:33:59 +0530

    Update max_of_three.py

--- practicepython.org/max_of_three.py
+++ practicepython.org/max_of_three.py
@@ -1,5 +1,6 @@
 # Ques)Implement a function that takes as input three variables, and returns the largest of the three. Do this without using the Python max() function!
 # The goal of this exercise is to think about some internals that Python normally takes care of for us. All you need is some variables and if statements!
+# Solution-
 
 def max_of_three(a,b,c):
 	if a>=b and a>=c:
