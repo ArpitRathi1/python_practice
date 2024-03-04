a642c32ce7b8be0b0dff82b586483c4da1de070d
ArpitRathi1 <rathi844@gmail.com>
2024-03-04 18:36:03 +0530

    reverse word order

---
+++ practicepython.org/reverse_word_order.py
@@ -0,0 +1,17 @@
+# Ques) Write a program functions that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+# "My name is Michele"
+# Then I would see the string:
+# "Michele is name My"
+# shown back to me.
+
+def reverse_word_order(string):
+	lst=string.split()
+	new_string=""
+	for i in reversed(lst):
+		new_string=new_string+i+" "
+	return new_string
+
+
+string=input("Enter your string : ")
+output=reverse_word_order(string)
+print(output)
