114e56108b4c7adbbb992688924a262204afc8cf
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-03-06 13:04:08 +0530

    Update f_string.py

--- practicepython.org/f_string.py
+++ practicepython.org/f_string.py
@@ -1,4 +1,5 @@
 # Ques) Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old using f-strings instead of the + operator to print the resulting output message.
+# Solution-
 
 name=input("Enter your name : ")
 age=int(input("Enter is your age : "))
