537779190732f35ca1adde85ffb1fd45be19b7e7
ArpitRathi1 <151003878+ArpitRathi1@users.noreply.github.com>
2024-03-07 12:38:22 +0530

    Update guessing_game.py

--- practicepython.org/guessing_game.py
+++ practicepython.org/guessing_game.py
@@ -1,4 +1,4 @@
-# Ques)Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.
+# Ques) Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.
 # Extras:
 # 1)Ask user if he want to play agian or not.
 # 2)Keep track of how many guesses the user has taken, and when the game ends, print this out.
