a19b001026c4ff0314916a9ecfa4709af349516b
ArpitRathi1 <rathi844@gmail.com>
2024-03-11 13:37:47 +0530

    added hangman part 1

---
+++ practicepython.org/hangman/part_1_pick_word.py
@@ -0,0 +1,13 @@
+# Ques) In this exercise, the task is to write a function that picks a random word from a list of words from the SOWPODS dictionary. Download this file and save it in the same directory as your Python code. This file is Peter Norvig’s compilation of the dictionary of words used in professional Scrabble tournaments. Each line in the file contains a single word.
+import random
+
+def pick_word():
+	sowpods_list=[]
+	f1=open("sowpods.txt","r")
+	line=f1.readlines()
+	for i in line:
+		a=i.replace("\n","")
+		sowpods_list.append(a)
+	random_choice=random.choice(sowpods_list)
+	return random_choice
+ 
