b244dfa57437d15163ec28a3c2e4477c58b996a6
ArpitRathi1 <rathi844@gmail.com>
2024-03-14 12:17:45 +0530

    added tic tac toe part 1

---
+++ practicepython.org/tic_tac_toe/part_1_draw_a_game_board.py
@@ -0,0 +1,22 @@
+# Ques)Time for some fake graphics! Let’s say we want to draw game boards that look like this:
+#  --- --- --- 
+# |   |   |   | 
+#  --- --- ---  
+# |   |   |   | 
+#  --- --- ---  
+# |   |   |   | 
+#  --- --- --- 
+# This one is 3x3 (like in tic tac toe). Obviously, they come in many other sizes (8x8 for chess, 19x19 for Go, and many more).
+
+
+def drow_horizontal_line():
+	print(" ---" * n)
+def drow_vertical_line():
+	print("|   " * (n+1))
+
+
+n=int(input("Enter size of game board : "))
+for i in range (n):
+	drow_horizontal_line()
+	drow_vertical_line()
+drow_horizontal_line()
