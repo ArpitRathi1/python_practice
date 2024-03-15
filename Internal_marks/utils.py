33d9dd14f4701c0344ae9f14621c4bea05192016
ArpitRathi1 <rathi844@gmail.com>
2024-03-15 12:48:36 +0530

    update utils

--- Internal_marks/utils.py
+++ Internal_marks/utils.py
@@ -4,3 +4,21 @@ def string_check(string):
 			return True
 		else:
 			return False
+
+def add_entry():
+	"""This function is created for addinf new enteries in internal_marks.txt file."""
+	name_of_student = input("Enter Name of the Student : ")
+	mark_in_internal_one = int(input("Enter First internal marks of the Student : "))
+	mark_in_internal_two = int(input("Enter First internal marks of the Student : "))
+	mark_in_internal_three = int(input("Enter First internal marks of the Student : "))
+	if mark_in_internal_one>20 or mark_in_internal_two>20 or mark_in_internal_three>20 :
+		print("Invalid Enter")
+	else:
+		if string_check(name_of_student):
+			f1 = open("internal_marks.txt","a")
+			entry = "{},{},{},{}\n".format(name_of_student,mark_in_internal_one,mark_in_internal_two,mark_in_internal_three)
+			f1.write(entry)
+			f1.close()
+			print("Student Marks were added Successfully")
+		else:
+			print("Invalid Enter")
