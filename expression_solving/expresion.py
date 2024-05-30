020e7615c140461dee7c4b761e9af4f3903b9942
ArpitRathi1 <rathi844@gmail.com>
2024-05-30 16:56:33 +0530

    update expresion

--- expression_solving/expresion.py
+++ expression_solving/expresion.py
@@ -1,3 +1,8 @@
 class Expression:
 	def __init__(self):
 		self.input_str = input("Enter Expression to solve : ")
+	def split_str(self):
+		operator_list = ["+","-","*","/","%","(",")"]
+		operator = ""
+		operand = ""
+		expression_list=[]
