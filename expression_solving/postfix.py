73b23bc3679fa6cf322aaaf0692ca57b9f22a672
ArpitRathi1 <rathi844@gmail.com>
2024-05-31 11:47:13 +0530

    update postfix

--- expression_solving/postfix.py
+++ expression_solving/postfix.py
@@ -5,3 +5,12 @@ def postfix(expresion):
 	operator_priority = {"+":1,"-":1,"*":2,"/":2,"(":0,}
 	temp_stack = tech_stack()
 	post_stack = tech_stack()
+	for i in expresion:
+		if i == "(":
+			temp_stack.push(i)
+		
+		elif i == ")":
+			while temp_stack.top_value() != "(":
+				item = temp_stack.pop()
+				post_stack.push(item)
+			temp_stack.pop()
