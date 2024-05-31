47dddd84f0b55c1379368e8f7a2925c96fafde17
ArpitRathi1 <rathi844@gmail.com>
2024-05-31 11:46:42 +0530

    update postfix

--- expression_solving/postfix.py
+++ expression_solving/postfix.py
@@ -1,2 +1,7 @@
 from expresion import *
 from tech_stack import *
+
+def postfix(expresion):
+	operator_priority = {"+":1,"-":1,"*":2,"/":2,"(":0,}
+	temp_stack = tech_stack()
+	post_stack = tech_stack()
