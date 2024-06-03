b502f45e3d9b33e77b5d3b02f1d45f252a18b129
ArpitRathi1 <rathi844@gmail.com>
2024-06-03 17:17:09 +0530

    update solution

--- expression_solving/solution.py
+++ expression_solving/solution.py
@@ -5,3 +5,6 @@ from postfix import postfix
 def solution(postfix_ex):
 	solution_stack = tech_stack()
 	operator_priority = {"+":1,"-":1,"*":2,"/":2}
+	for i in postfix_ex:
+		if i not in operator_priority:
+			solution_stack.push(i)
