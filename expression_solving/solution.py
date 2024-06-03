7bad8ddda6ac722086f3e51d05de64bc4d4d46c8
ArpitRathi1 <rathi844@gmail.com>
2024-06-03 17:16:40 +0530

    update solution

--- expression_solving/solution.py
+++ expression_solving/solution.py
@@ -1,3 +1,7 @@
 from expresion import *
 from tech_stack import *
 from postfix import postfix
+
+def solution(postfix_ex):
+	solution_stack = tech_stack()
+	operator_priority = {"+":1,"-":1,"*":2,"/":2}
